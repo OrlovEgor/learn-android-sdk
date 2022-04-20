b26002e7139e2293c0a678c4b31726637a108c4c
Orlov Egor <dbn.orlov@gmail.com>
2022-04-20 23:15:16 +0300

    Created nav_graph and fragments

---
+++ Notification,Firebase and Broadcast receiver/app/src/main/java/ru/orlovegor/notificationapp/ReceiveFragment.kt
@@ -0,0 +1,19 @@
+package ru.orlovegor.notificationapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import by.kirich1409.viewbindingdelegate.viewBinding
+import ru.orlovegor.notificationapp.databinding.FragmentReceiveBinding
+
+
+class ReceiveFragment : Fragment(R.layout.fragment_receive) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

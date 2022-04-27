2e6113fb31074123bd0802ff1ba7710bf0753316
Orlov Egor <dbn.orlov@gmail.com>
2022-04-28 00:03:00 +0300

    initial

--- Notification,Firebase and Broadcast receiver/app/src/main/java/ru/orlovegor/notificationapp/MainActivity.kt
+++ Notification,Firebase and Broadcast receiver/app/src/main/java/ru/orlovegor/notificationapp/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.orlovegor.notificationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

735a0bb07cf91f7de9b7b123b1dd3f03ca2d1aeb
Orlov Egor <dbn.orlov@gmail.com>
2022-04-28 00:03:41 +0300

    add logic to get token

--- Notification,Firebase and Broadcast receiver/app/src/main/java/ru/orlovegor/notificationapp/MainFragment.kt
+++ Notification,Firebase and Broadcast receiver/app/src/main/java/ru/orlovegor/notificationapp/MainFragment.kt
@@ -2,11 +2,39 @@ package ru.orlovegor.notificationapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class MainFragment : Fragment(R.layout.fragment_main) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val tokenTextView = view.findViewById<TextView>(R.id.token_text_view)
+        setTokenToTextView()
     }
 
+    private fun setTokenToTextView() {
+        val tokenTextView = view?.findViewById<TextView>(R.id.token_text_view)
+        lifecycleScope.launch {
+            tokenTextView?.text = getToken()
+        }
+    }
+
+    private suspend fun getToken(): String? = suspendCoroutine { continuation ->
+        FirebaseMessaging.getInstance().token
+            .addOnSuccessListener { token ->
+                continuation.resume(token)
+            }
+            .addOnFailureListener { exception ->
+                continuation.resume(null)
+            }
+            .addOnCanceledListener {
+                continuation.resume(null)
+            }
+    }
 }

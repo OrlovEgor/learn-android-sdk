8b20535b993899064fc53d159bf1c8687ab49045
Orlov Egor <dbn.orlov@gmail.com>
2022-04-28 00:02:34 +0300

    add messaging services firebase

---
+++ Notification,Firebase and Broadcast receiver/app/src/main/java/ru/orlovegor/notificationapp/MessagingService.kt
@@ -0,0 +1,15 @@
+package ru.orlovegor.notificationapp
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MessagingService: FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+    }
+}

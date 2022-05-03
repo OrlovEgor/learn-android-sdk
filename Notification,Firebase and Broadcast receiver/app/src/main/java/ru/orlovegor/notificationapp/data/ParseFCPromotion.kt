eea67966f59788b476c0be9fd052a123327b460c
Orlov Egor <dbn.orlov@gmail.com>
2022-05-04 01:23:12 +0300

    created parsers

---
+++ Notification,Firebase and Broadcast receiver/app/src/main/java/ru/orlovegor/notificationapp/data/ParseFCPromotion.kt
@@ -0,0 +1,17 @@
+package ru.orlovegor.notificationapp.data
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+
+interface ParseFCPromotion {
+    fun parseDate(message: String): NewPromotions?
+
+    class Base : ParseFCPromotion {
+        override fun parseDate(message: String): NewPromotions? {
+            val moshi = Moshi.Builder().build()
+            val newPromotionsAdapter: JsonAdapter<NewPromotions> =
+                moshi.adapter(NewPromotions::class.java)
+            return newPromotionsAdapter.fromJson(message)
+        }
+    }
+}

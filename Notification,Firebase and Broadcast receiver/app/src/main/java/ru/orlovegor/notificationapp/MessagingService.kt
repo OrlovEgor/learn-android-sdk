66022b4a5fd0516a53e67e388e190475e8bd9859
Orlov Egor <dbn.orlov@gmail.com>
2022-05-05 01:44:16 +0300

    refactoring deleted unused import

--- Notification,Firebase and Broadcast receiver/app/src/main/java/ru/orlovegor/notificationapp/MessagingService.kt
+++ Notification,Firebase and Broadcast receiver/app/src/main/java/ru/orlovegor/notificationapp/MessagingService.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.media.RingtoneManager
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -12,9 +11,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.target.Target
-import com.bumptech.glide.request.transition.Transition
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import ru.orlovegor.notificationapp.data.*

2b5959ca1f453b1cd415c8edb46c870e8c41b8a7
Orlov Egor <dbn.orlov@gmail.com>
2022-06-06 20:30:55 +0300

    added nav graph and blank fragment

---
+++ Flow,Retrofit,DAO Room/app/src/main/java/ru/orlovegor/moviesearchapp/MovieSearchFragment.kt
@@ -0,0 +1,12 @@
+package ru.orlovegor.moviesearchapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class MovieSearchFragment: Fragment(R.layout.fragment_movie_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

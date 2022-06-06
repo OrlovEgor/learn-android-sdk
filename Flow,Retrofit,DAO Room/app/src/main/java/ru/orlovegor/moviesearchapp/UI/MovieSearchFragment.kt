1bea1df30dd6e1bca3bf7fe39113bf2a12ed1df9
Orlov Egor <dbn.orlov@gmail.com>
2022-06-06 23:31:55 +0300

    Created empty movie fragment

--- Flow,Retrofit,DAO Room/app/src/main/java/ru/orlovegor/moviesearchapp/UI/MovieSearchFragment.kt
+++ Flow,Retrofit,DAO Room/app/src/main/java/ru/orlovegor/moviesearchapp/UI/MovieSearchFragment.kt
@@ -1,8 +1,9 @@
-package ru.orlovegor.moviesearchapp
+package ru.orlovegor.moviesearchapp.UI
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import ru.orlovegor.moviesearchapp.R
 
 class MovieSearchFragment: Fragment(R.layout.fragment_movie_search) {
 

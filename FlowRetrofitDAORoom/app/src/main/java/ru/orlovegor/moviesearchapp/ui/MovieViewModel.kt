fe80cedd98d11db95431be4948e4f2f690c4d0b3
Orlov Egor <dbn.orlov@gmail.com>
2022-06-30 23:59:58 +0300

    change VM logic

--- FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/ui/MovieViewModel.kt
+++ FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/ui/MovieViewModel.kt
@@ -25,31 +25,22 @@ class MovieViewModel : ViewModel() {
         get() = _listMovie
 
 
-    fun bind(queryFlow: Flow<String>, movieTypeFlow: Flow<MovieTypes>) {
-        scope = CoroutineScope(Dispatchers.Main + SupervisorJob())
+        fun bind(queryFlow: Flow<String>, movieTypeFlow: Flow<MovieTypes>) {
+        scope = CoroutineScope(Dispatchers.IO )
         combine(
             queryFlow,
             movieTypeFlow
         )
         { query, type -> query to type }
-            .catch { it ->
-                Log.d("TAG", "errorBind0 = $it}")
-                _toast.postValue(R.string.input_error)
-            }
             .debounce(2000)
             .onEach { Log.d("TAG", "${it.first}, ${it.second.name}") }
-            .catch { it ->
-                Log.d("TAG", "errorBind1 = $it}")
-                _toast.postValue(R.string.input_error)
-            }
             .flowOn(Dispatchers.IO)
-            .mapLatest { _listMovie.postValue(repository.getMovie(it.first, it.second.name.lowercase())) }
-            /*.catch {
+            .mapLatest {
+                _listMovie.postValue(repository.getMovie(it.first, it.second.name.lowercase())) }
+            .catch {
                 Log.d("TAG", "errorBind2 = $it}")
                 _toast.postValue(R.string.input_error)
-            }*/
-            .onEach { Log.d("Tag", " after error 2 value = $it") }
-           // .catch { Log.d("TAG", "errorBind3 = $it}")  }
+            }
             .launchIn(scope!!)
 
     }

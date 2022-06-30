e1fef42da3f4dff255652b43ca000af084fe9a9f
Orlov Egor <dbn.orlov@gmail.com>
2022-06-30 23:58:34 +0300

    catch json data exception

--- FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/data/Repository.kt
+++ FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/data/Repository.kt
@@ -1,13 +1,16 @@
 package ru.orlovegor.moviesearchapp.data
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import android.util.Log
+import com.squareup.moshi.JsonDataException
 
 import ru.orlovegor.moviesearchapp.networking.Network
 
 class Repository {
 
-    suspend fun getMovie(tittle: String, type: String): List<RemoteMovie> =
-         Network.movieApi.getMovieByTittle(tittle, type)
-
+    suspend fun getMovie(tittle: String, type: String): List<RemoteMovie> = try {
+        Network.movieApi.getMovieByTittle(tittle, type)
+    } catch (j: JsonDataException) {
+        Log.d("TAG", " Json data exception = $j")
+        listOf<RemoteMovie>()
+    }
 }

983dcb369786b3393f5eb21be1d5ed039e80cf03
Orlov Egor <dbn.orlov@gmail.com>
2022-07-04 23:07:10 +0300

    Add application class to manifest

--- FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/Application.kt
+++ FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/Application.kt
@@ -1,9 +1,11 @@
 package ru.orlovegor.moviesearchapp
 
 import android.app.Application
+import ru.orlovegor.moviesearchapp.data.dao.Database
 
-class Application: Application() {
+class Application : Application() {
     override fun onCreate() {
         super.onCreate()
+        Database.init(this)
     }
 }

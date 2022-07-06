1f59e6a0e7a007c036e1a3a58d9c270c018affc2
Orlov Egor <dbn.orlov@gmail.com>
2022-07-06 23:11:03 +0300

    Add logic: loadMovie movies

--- FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/data/RemoteDataSource.kt
+++ FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/data/RemoteDataSource.kt
@@ -1,4 +1,9 @@
 package ru.orlovegor.moviesearchapp.data
 
+import ru.orlovegor.moviesearchapp.networking.Network
+
 class RemoteDataSource {
+    suspend fun loadMovie(tittle: String, type: String) =
+        Network.movieApi.getMovieByTittle(tittle, type)
+
 }

bfa30601f31f47080a3d717e0a8b646f58024e20
Orlov Egor <dbn.orlov@gmail.com>
2022-07-06 23:39:28 +0300

    Add Query param. getMovies

--- FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/data/dao/MovieDao.kt
+++ FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/data/dao/MovieDao.kt
@@ -3,10 +3,16 @@ package ru.orlovegor.moviesearchapp.data.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-
+import androidx.room.Query
+import ru.orlovegor.moviesearchapp.data.dao.contract.MovieContract
+import ru.orlovegor.moviesearchapp.data.models.LocalMovie
+import ru.orlovegor.moviesearchapp.data.models.MovieTypes
 
 @Dao
 interface MovieDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(movie: List<LocalMovie>)
+
+    @Query("SELECT * FROM ${MovieContract.TABLE_NAME} WHERE ${MovieContract.Columns.TITTLE} LIKE '%' || :tittle || '%' AND ${MovieContract.Columns.MOVIE_TYPE} = :type")
+    fun getMovies(tittle: String, type: MovieTypes): List<LocalMovie>
 }

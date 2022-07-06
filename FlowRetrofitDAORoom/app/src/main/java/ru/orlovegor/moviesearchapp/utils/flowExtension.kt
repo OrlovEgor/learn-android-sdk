e09921c9c4b5234d770881636c0635e0122f85b1
Orlov Egor <dbn.orlov@gmail.com>
2022-07-07 01:47:03 +0300

    delete un usage import

--- FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/utils/flowExtension.kt
+++ FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/utils/flowExtension.kt
@@ -5,17 +5,11 @@ import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
 import android.widget.RadioGroup
-import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.onFailure
-import kotlinx.coroutines.channels.onSuccess
 import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
-import ru.orlovegor.moviesearchapp.R
-import ru.orlovegor.moviesearchapp.data.MovieTypes
 
 fun EditText.textChangedFlow(): Flow<String> {
     return callbackFlow {
@@ -25,9 +19,7 @@ fun EditText.textChangedFlow(): Flow<String> {
             override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 trySendBlocking(s?.toString().orEmpty()
                 ).onFailure { Log.d("TAG", "error textChangedFlow") }
-
             }
-
             override fun afterTextChanged(p0: Editable?) {}
         }
         this@textChangedFlow.addTextChangedListener(textChangedListener)
@@ -50,20 +42,3 @@ fun RadioGroup.checkedChangesFlow(): Flow<Int> {
     }
 }
 
-
-/*private fun flowFromGroupChanged(): Flow<MovieTypes> {
-    viewLifecycleOwner.lifecycleScope.launch {
-        Log.d("TAG", "fragment flowGroupChanged start")
-        callbackFlow {
-            binding.movieTypeRadioGroup.checkedChangesFlow()
-                .map {
-                    when (it) {
-                        R.id.radio_button_movie -> MovieTypes.MOVIE
-                        R.id.radio_button_episode -> MovieTypes.EPISODE
-                        R.id.radio_button_series -> MovieTypes.SERIES
-                        else -> {}
-                    }
-                }
-        }
-    }
-}*/

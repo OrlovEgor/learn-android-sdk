b5cd49fcff9a2db695cfef4edd202bf375701054
Orlov Egor <dbn.orlov@gmail.com>
2022-07-07 01:45:59 +0300

    Add val movie type

--- FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/adapters/MovieParseAdapter.kt
+++ FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/adapters/MovieParseAdapter.kt
@@ -4,13 +4,21 @@ package ru.orlovegor.moviesearchapp.adapters
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import ru.orlovegor.moviesearchapp.data.RemoteMovie
+import ru.orlovegor.moviesearchapp.data.models.MovieTypes
+import ru.orlovegor.moviesearchapp.data.models.RemoteMovie
 
 class MovieParseAdapter {
 
     @FromJson
     fun fromJson(search: Search): List<RemoteMovie> =
-        search.listMovie.map { movie -> RemoteMovie(movie.id, movie.title, movie.image) }
+        search.listMovie.map { movie ->
+            RemoteMovie(
+                movie.id,
+                movie.title,
+                movie.movieType,
+                movie.image
+            )
+        }
 
     @JsonClass(generateAdapter = true)
     data class Movie(
@@ -18,6 +26,8 @@ class MovieParseAdapter {
         val id: String,
         @Json(name = "Title")
         val title: String,
+        @Json(name = "Type")
+        val movieType: MovieTypes,
         @Json(name = "Poster")
         val image: String?
     )

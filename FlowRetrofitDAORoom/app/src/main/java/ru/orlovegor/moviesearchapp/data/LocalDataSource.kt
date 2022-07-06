16b60c15321293a0869745a30ccb6ead0d70aff0
Orlov Egor <dbn.orlov@gmail.com>
2022-07-06 23:09:47 +0300

    Add logic: insert movies

--- FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/data/LocalDataSource.kt
+++ FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/data/LocalDataSource.kt
@@ -1,4 +1,15 @@
 package ru.orlovegor.moviesearchapp.data
 
+import ru.orlovegor.moviesearchapp.data.dao.Database
+import ru.orlovegor.moviesearchapp.data.models.LocalMovie
+import ru.orlovegor.moviesearchapp.data.models.MovieTypes
+
 class LocalDataSource {
+    private val movieDao = Database.instance.movieDao()
+
+     fun insertMovies(movie: List<LocalMovie>) {
+        movieDao.insert(movie)
+    }
+
+    fun getMovie(tittle: String, type: MovieTypes) = movieDao.getMovies(tittle, type)
 }

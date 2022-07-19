4cdad7b50de550062407f7b497b38ef24e509a59
Orlov Egor <dbn.orlov@gmail.com>
2022-07-19 23:56:48 +0300

    refactoring: delete empty line

--- FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/utils/ResultWrapper.kt
+++ FlowRetrofitDAORoom/app/src/main/java/ru/orlovegor/moviesearchapp/utils/ResultWrapper.kt
@@ -4,5 +4,4 @@ sealed class ResultWrapper<out T> {
     data class Success<out T>(val value: T) : ResultWrapper<T>()
     data class Error(val exception: Throwable?) : ResultWrapper<Nothing>()
     object NetworkError : ResultWrapper<Nothing>()
-
 }

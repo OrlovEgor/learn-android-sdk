d9c254ae2675beeafa7c1f122435859b7fa06255
Orlov Egor <dbn.orlov@gmail.com>
2022-08-04 22:37:28 +0300

    Change logic

--- ServicesWorkManager/app/src/main/java/ru/orlovegor/workmanagerandservices/presenter/MainViewModel.kt
+++ ServicesWorkManager/app/src/main/java/ru/orlovegor/workmanagerandservices/presenter/MainViewModel.kt
@@ -3,11 +3,10 @@ package ru.orlovegor.workmanagerandservices.presenter
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.asFlow
 import androidx.lifecycle.viewModelScope
 import androidx.work.WorkInfo
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
+import ru.orlovegor.workmanagerandservices.R
 import ru.orlovegor.workmanagerandservices.data.Repository
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
@@ -17,26 +16,55 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val _error = MutableStateFlow(false)
     private val _progress = MutableStateFlow(false)
 
+    private val _workerStatus: Flow<MutableList<WorkInfo>> = repo.observeWork()
+
     val toast = _toast.asSharedFlow()
     val error = _error.asStateFlow()
     val progress = _progress.asStateFlow()
 
-    fun downloadFile(uri: String){
-        val status =   repo.startDownload(uri).asFlow()
-        viewModelScope.launch {
-            status.onEach{ workInfo ->
-                when(workInfo.first().state){
-                    WorkInfo.State.RUNNING -> Log.d("TAG", "RUNNING")
-                    WorkInfo.State.BLOCKED -> Log.d("TAG", "BLOCKED")
-                    WorkInfo.State.CANCELLED -> Log.d("TAG", "CANCELED")
-                    WorkInfo.State.ENQUEUED -> Log.d("TAG", "ENQUEUED")
-                    WorkInfo.State.SUCCEEDED -> Log.d("TAG", "SUCCEEDED")
-                    WorkInfo.State.FAILED ->Log.d("TAG", "FAILED")
-                }
-            }
-        }
+    init {
+        check()
     }
-    fun stopDownload(){
+
+    private fun check() {
+        _workerStatus.map { listWorkInfo ->
+            if (listWorkInfo.isNotEmpty())
+                when (listWorkInfo.first().state) {
+                    WorkInfo.State.RUNNING -> {
+                        Log.d("TAG", "RUNNING")
+                    }
+                    WorkInfo.State.BLOCKED -> Log.d("TAG", "BLOCKED")
+                    WorkInfo.State.CANCELLED -> {
+                        Log.d("TAG", "CANCELED")
+                        _progress.emit(false)
+                        _error.emit(false)
+                    }
+                    WorkInfo.State.ENQUEUED -> {
+                        Log.d("TAG", "ENQUEUED")
+                        _progress.emit(true)
+                    }
+                    WorkInfo.State.SUCCEEDED -> {
+                        Log.d("TAG", "SUCCEEDED")
+                        _toast.emit(R.string.success)
+                        _progress.emit(false)
+                    }
+                    WorkInfo.State.FAILED -> {
+                        Log.d("TAG", "FAILED")
+                        _progress.emit(false)
+                        _error.emit(true)
+                    }
+                }
+
+        }
+            .onEach { repo.cleanWorkHistory() }
+            .launchIn(viewModelScope)
+    }
+
+    fun downloadFile(uri: String) {
+        repo.startDownload(uri)
+    }
+
+    fun stopDownload() {
         repo.stopDownload()
     }
 }

a29cf9cec99236cdaee3e96febba848077fdd07b
Orlov Egor <dbn.orlov@gmail.com>
2022-08-04 22:34:35 +0300

    deleted class Application

--- ServicesWorkManager/app/src/main/java/ru/orlovegor/workmanagerandservices/Application.kt
+++
@@ -1,9 +0,0 @@
-package ru.orlovegor.workmanagerandservices
-
-import android.app.Application
-
-class Application: Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}

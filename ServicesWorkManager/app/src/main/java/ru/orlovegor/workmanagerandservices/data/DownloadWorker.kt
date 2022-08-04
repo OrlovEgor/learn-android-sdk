0c5ceeb21a958b2676571016d576c10ab1573d4d
Orlov Egor <dbn.orlov@gmail.com>
2022-08-04 22:36:52 +0300

    Transfer fun createFile and downloadFile to Repository. Create fun observe work and use this in presenter. Create fun cleanWorkHistory

--- ServicesWorkManager/app/src/main/java/ru/orlovegor/workmanagerandservices/data/DownloadWorker.kt
+++ ServicesWorkManager/app/src/main/java/ru/orlovegor/workmanagerandservices/data/DownloadWorker.kt
@@ -1,54 +1,29 @@
 package ru.orlovegor.workmanagerandservices.data
 import android.content.Context
-import android.os.Environment
 import android.util.Log
 import androidx.work.CoroutineWorker
-import androidx.work.WorkManager
 import androidx.work.WorkerParameters
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.io.File
-import java.lang.RuntimeException
+import retrofit2.HttpException
+
 
 class DownloadWorker(
     context: Context,
-    params: WorkerParameters
+    params: WorkerParameters,
 ) : CoroutineWorker(context, params) {
+    private val repo = Repository(context)
     override suspend fun doWork(): Result {
         val uriToDownload = inputData.getString(DOWNLOAD_URL_KEY)
-        if (uriToDownload != null) {
-            download(createFile(uriToDownload), uriToDownload)
-        } else return Result.failure()
-        Log.d("TAG", "WORK START")
-        return Result.success()
-    }
-
-    private suspend fun createFile(url: String): File {
-        return withContext(Dispatchers.IO) {
-            if (Environment.getExternalStorageState() != Environment.MEDIA_MOUNTED) {
-                throw RuntimeException()
-            } else {
-                val folder =
-                    applicationContext.getExternalFilesDir("external storage/files/File folder")
-                val fileName =
-                    "${System.currentTimeMillis()}_${url.substringAfterLast("/", "")}"
-                val file = File(folder, fileName)
-                return@withContext (file)
-            }
-        }
-    }
-
-    private suspend fun download(file: File, url: String) {
-        return withContext(Dispatchers.IO) {
-            if (Environment.getExternalStorageState() != Environment.MEDIA_MOUNTED) return@withContext
-            file.outputStream().use { fileOutputStream ->
-                Networking.downloadApi
-                    .getFile(url)
-                    .byteStream()
-                    .use { inputStream ->
-                        inputStream.copyTo(fileOutputStream)
-                    }
-            }
+        return try {
+            Log.d("TAG", "WORK START")
+            if (uriToDownload != null) {
+               repo.download(repo.createFile(uriToDownload), uriToDownload)
+                Result.success()
+            } else Result.failure()
+        } catch (h: HttpException) {
+            Log.d("TAG", "Error = HttpException")
+            Result.retry()
+        } catch (t: Throwable) {
+            Result.failure()
         }
     }
 
